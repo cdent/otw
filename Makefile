3f7a12fa3292a8d33a116a7e1f8e65d707943c6e
Chris Dent <chris.dent@gmail.com>
2011-10-26 23:28:32 +0100

    Add a very limited test just to make sure things are in place.
    Need much better dependency management in the Makefile.

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
+.PHONY: clean dev resources remotes test
 
 RESOURCES_DIR=tiddlywebplugins/otw/resources
 SOURCES_DIR=sources
 
 clean:
-	rm -rf $(RESOURCES_DIR)/* || true
+	rm -rf $(RESOURCES_DIR) || true
 	rm $(SOURCES_DIR)/*.js || true
 	find . -name "*.pyc" -exec rm {} \; || true
 
@@ -17,6 +18,7 @@ dev:
 	echo "start up the server and visit http://0.0.0.0:8080/bags/stuff/tiddlers.otw")
 
 resources: remotes
+	mkdir -p $(RESOURCES_DIR) || true
 	cook $$PWD/lib/otw.recipe -d $$PWD/$(RESOURCES_DIR) -o otw.html
 	cook $$PWD/lib/otw.js.recipe -d $$PWD/$(SOURCES_DIR) -o otw.js.js -j
 	./cacher
@@ -24,3 +26,6 @@ resources: remotes
 remotes:
 	wget https://raw.github.com/TiddlyWiki/tiddlywiki/master/jquery/jquery.js -O $(SOURCES_DIR)/jquery.js.js
 	wget https://raw.github.com/TiddlyWiki/tiddlywiki/master/jquery/plugins/jQuery.twStylesheet.js -O $(SOURCES_DIR)/jQuery.twStylesheet.js.js
+
+test:
+	py.test -x test
---
+++ test/test_web_otw.py
@@ -0,0 +1,43 @@
+
+import os
+import shutil
+import wsgi_intercept
+import httplib2
+
+from wsgi_intercept import httplib2_intercept
+
+from tiddlywebplugins.utils import get_store
+
+from tiddlyweb.config import config
+from tiddlyweb.model.tiddler import Tiddler
+from tiddlyweb.model.bag import Bag
+
+def setup_module(module):
+    from tiddlyweb.web import serve
+    def app_fn():
+        return serve.load_app()
+    httplib2_intercept.install()
+    wsgi_intercept.add_wsgi_intercept('0.0.0.0', 8000, app_fn)
+
+    if os.path.exists('store'):
+        shutil.rmdir('store')
+
+    store = get_store(config)
+    module.http = httplib2.Http()
+
+    store.put(Bag('stuff'))
+    module.store = store
+
+
+def test_get_wiki():
+    response, content = http.request(
+            'http://0.0.0.0:8000/bags/stuff/tiddlers.wiki')
+
+    response['status'] == '200'
+    assert 'otw.js' not in content
+
+    response, content = http.request(
+            'http://0.0.0.0:8000/bags/stuff/tiddlers.otw')
+
+    response['status'] == '200'
+    assert 'otw.js' in content
