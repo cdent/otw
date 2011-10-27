184b96f866fe4dcd00c0143fdb88e0d0e4d84fc7
Chris Dent <chris.dent@gmail.com>
2011-10-27 11:51:31 +0100

    add tiddlywebconfig.py so tests work

---
+++ tiddlywebconfig.py
@@ -0,0 +1,5 @@
+config = {
+        'log_level': 'DEBUG',
+        'system_plugins': ['tiddlywebwiki', 'tiddlywebplugins.otw'],
+        'twanager_plugins': ['tiddlywebwiki', 'tiddlywebplugins.otw'],
+}
